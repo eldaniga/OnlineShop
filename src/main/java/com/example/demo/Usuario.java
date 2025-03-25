24ba59da098c6add29ee055660827b39979ff3bb
Erik Daniel Guerra Alvarez <155863998+eldaniga@users.noreply.github.com>
2025-03-25 11:26:35 +0100

    Adding new classes

---
+++ src/main/java/com/example/demo/Usuario.java
@@ -0,0 +1,57 @@
+public class Usuario {
+    private String email;
+    private String password;
+    private String nombre;
+    private String apellidos;
+
+    // Constructor
+    public Usuario(String email, String password, String nombre, String apellidos) {
+        this.email = email;
+        this.password = password;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    // Getters
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    // Setters
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "email='" + email + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellidos='" + apellidos + '\'' +
+                '}';
+    }
+}
